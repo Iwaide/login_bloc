9a0ac2f2c7bd96217ed0e2e28e4c7ccaf939068d
Iwaide <iwaide@hoobby.net>
2020-01-18 18:56:19 +0900

    Add getter and setter

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,5 +1,12 @@
 import 'dart:async';
 
 class Bloc {
-  
+  final _email = StreamController<String>();
+  final _password = StreamController<String>();
+
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _password.sink.add;
+
+  Stream<String> get email => _email.stream;
+  Stream<String> get password => _password.stream;
 }
