20947c84eba660270db97d9ad726aaac340fee12
Iwaide <iwaide@hoobby.net>
2020-01-18 19:11:02 +0900

    Add dispose

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -8,6 +8,11 @@ class Bloc extends Object with Validators {
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
 
-  Stream<String> get email => _email.stream;
-  Stream<String> get password => _password.stream;
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _password.stream.transform(validatePassword);
+
+  dispose() {
+    _email.close();
+    _password.close();
+  }
 }
